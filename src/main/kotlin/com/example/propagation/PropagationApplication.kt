6a3a292bcdeb9f810bf7a9dd0125e674b24ddaea
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2023-11-04 15:40:42 +0900

    init

---
+++ src/main/kotlin/com/example/propagation/PropagationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.propagation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PropagationApplication
+
+fun main(args: Array<String>) {
+	runApplication<PropagationApplication>(*args)
+}
